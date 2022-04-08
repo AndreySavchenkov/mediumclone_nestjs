f4e5a08eedb65e300b5e5cb6182775b6b10a0d9d
sandvik16 <sannndvik16@gmail.com>
2022-04-08 15:29:06 +0300

    feat: middleware for authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,27 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TagModule } from 'src/tag/tag.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import ormconfing from 'src/ormconfig';
 import { UserModule } from 'src/user/user.module';
+import { AuthMiddleware } from 'src/user/middlewares/auth-middleware.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot(ormconfing), TagModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
