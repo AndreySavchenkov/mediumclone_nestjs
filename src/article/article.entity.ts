ad70f3c0a486e1b5718a0fc40eeffc7422a4465a
sandvik16 <sannndvik16@gmail.com>
2022-04-11 10:58:03 +0300

    feat: delete single by slug with checked id author

--- src/article/article.entity.ts
+++ src/article/article.entity.ts
@@ -38,6 +38,6 @@ export class ArticleEntity {
     this.updatedAt = new Date();
   }
 
-  @ManyToOne(() => UserEntity, (user) => user.articles)
+  @ManyToOne(() => UserEntity, (user) => user.articles, { eager: true })
   author: UserEntity;
 }
