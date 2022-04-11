fece140699313ac55f436bd5a41ec3a27090f4d9
sandvik16 <sannndvik16@gmail.com>
2022-04-11 09:06:58 +0300

    feat: add method for added new post and relations between post and user

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ArticleService } from 'src/article/article.service';
+import { AuthGuard } from 'src/user/guards/auth.guard';
+import { UserEntity } from 'src/user/user.entity';
+import { User } from 'src/user/decorators/user.decorator';
+import { CreateArticleDto } from 'src/article/dto/createArticle.dto';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
   @Post()
-  async create() {
-    return this.articleService.createArticle();
+  @UseGuards(AuthGuard)
+  async create(
+    @User() currentUser: UserEntity,
+    @Body('article') createArticleDto: CreateArticleDto,
+  ): Promise<any> {
+    return this.articleService.createArticle(currentUser, createArticleDto);
   }
 }
--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ArticleController } from 'src/article/article.controller';
 import { ArticleService } from 'src/article/article.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ArticleEntity } from 'src/article/article.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ArticleEntity])],
   controllers: [ArticleController],
   providers: [ArticleService],
 })
