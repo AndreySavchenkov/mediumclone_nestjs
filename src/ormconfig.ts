2e3bf0513531c5a3114a6f51e833e27907e76c6b
sandvik16 <sannndvik16@gmail.com>
2022-04-12 18:46:24 +0300

    fix: change ormconfig.ts

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,13 +1,12 @@
 import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
-  url: process.env.DATABASE_URL,
   type: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
+  host: 'ec2-52-18-116-67.eu-west-1.compute.amazonaws.com',
+  database: 'd24168pfsunsg7',
   port: 5432,
-  username: 'postgres',
-  password: '123',
+  username: 'skbdvypscidviy',
+  password: 'f52bfcb439b05fe39dd52698aa082b7f8ad64af4964d40d05df694b316ca9b7b',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
